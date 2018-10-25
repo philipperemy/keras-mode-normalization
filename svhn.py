04dbb2456d8242a7987607ad443694f88e523e45
Philippe Rémy <premy@cogent.co.jp>
2018-10-26 08:49:55 +0900

    Update svhn.py

--- svhn.py
+++ svhn.py
@@ -6,10 +6,6 @@ import scipy.io as sio
 def load_data():
     """Loads the SVHN dataset.
 
-    # Arguments
-        path: path where to cache the dataset locally
-            (relative to ~/.keras/datasets).
-
     # Returns
         Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     """
